059c76b6f2a7a10b137f1494a99db3f8dfa5867d
Wares <fazelyar.wares@gmail.com>
2020-04-08 19:44:39 +0000

    added special keys to convey messages to server for everyone to see

--- input.js
+++ input.js
@@ -17,18 +17,18 @@ const setupInput = function(conn) {
 const handleUserInput = function(key) {
   if (key === ('\u0003')) {
     process.exit(console.log('Snake game over'));
-  }
-  if (key === 'w') {
+  } if (key === 'w') {
     connection.write('Move: up')
-  }
-  if (key === 'a') {
+  } if (key === 'a') {
     connection.write('Move: left')
-  }
-  if (key === 's') {
+  } if (key === 's') {
     connection.write('Move: down')
-  }
-  if (key === 'd') {
+  } if (key === 'd') {
     connection.write('Move: right')
+  } if (key === 'q') {
+    connection.write('Say: yum')
+  } if (key === 'r') {
+    connection.write('Say: GO!')
   }
 }
 
