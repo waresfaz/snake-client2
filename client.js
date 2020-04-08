0c88608e5a7cf156d92e49301a653fedaff35b4a
Wares <fazelyar.wares@gmail.com>
2020-04-08 18:26:04 +0000

    used setInterval to move snake

--- client.js
+++ client.js
@@ -10,7 +10,15 @@ const connect = function() {
 
   conn.on('connect', () => {
     console.log('Successfully connected!');
-    conn.write("Name: WF")
+    conn.write("Name: WF");
+    // conn.write("Move: up");
+    // conn.write("Move: left");
+    // setTimeout(() => {
+    //   conn.write("Move: up");
+    // }, 500);
+    setInterval(() => {
+      conn.write("Move: up");
+    }, 500);
   });
 
   conn.on('data', (data) => { // receives data from server; if snake idles serves send string to client (me)
